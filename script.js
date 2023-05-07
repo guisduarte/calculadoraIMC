d89ee198afa84aac33cddebff65c743b2058cfe5
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-05-06 21:04:23 -0300

    atualização

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-const altura = document.querySelector("#altura");
-const peso = document.querySelector("#peso");
+// Pegando os elementos HTML via DOM
 const botaoCalcular = document.querySelector(".botao-calcular");
 const resultado = document.querySelector(".resultado");
 const imagem = document.querySelector(".caixa-imagem");
+const seuImc = document.querySelector(".seu-imc");
 
+seuImc.style.display = "none";
 
-altura.value = "";
-peso.value = "";
+// Evento de click para aparecer "seu imc é:" ao clicar.
+botaoCalcular.addEventListener("click", () => {
+    if (seuImc.style.display = "none") {
+        seuImc.style.display = "block"
+    } else {
+        seuImc.style.display = "none"
+    }
+})
 
+// Evento de click passando afunção exibir
 botaoCalcular.addEventListener("click", exibir);
 
-// resultado.innerHTML = 0;
-
+// Função para fazer o calculo do IMC
 function calcularIMC (altura, peso) {
     return peso / (altura * altura);
 }
 
-function exibir() {
-    let inputAltura = document.getElementById("altura").value;
-    let inputPeso = document.getElementById("peso").value;
-    
-    let altura = parseFloat(inputAltura);
-    let peso = parseFloat(inputPeso);
+// Função para exibir o resultado
+function exibir() {   
+    let inputAltura = document.getElementById("altura");
+    let inputPeso = document.getElementById("peso");    
+
+    let altura = parseFloat(inputAltura.value);
+    let peso = parseFloat(inputPeso.value);
 
     resultado.innerHTML = calcularIMC(altura, peso).toFixed(2);
-
+        
     if (resultado.innerHTML < 18.5) {
-        imagem.innerHTML = ` <img src="img/abaixo-do-peso.png" alt="imagem abaixo do peso"> `
+        imagem.innerHTML = ` <img src="img/abaixo-do-peso.png" alt="imagem abaixo do peso">`        
     } else if (resultado.innerHTML >= 18.6 && resultado.innerHTML <= 24.99) {
        imagem.innerHTML = ` <img src="img/peso-normal.png" alt="imagem peso normal" > `
     } else if (resultado.innerHTML > 25 && resultado.innerHTML <= 29.99) {
@@ -38,8 +46,13 @@ function exibir() {
     } else if (resultado.innerHTML > 40) {
         imagem.innerHTML = ` <img src="img/obesidade-grau3.png" alt="imagem obesidade grau3" >  `
     } else {
+        imagem.innerHTML = "";
         resultado.innerHTML = "Resultado inválido!"
     }
+
+    // limpar os campos de input
+    inputAltura.value = "";
+    inputPeso.value = "";
 
 }
 
